58e18aee592ee4d44f5b14368f6c44cab06048a7
Dj_Mike238 <22237379+DjMike238@users.noreply.github.com>
2022-01-23 18:52:39 +0100

    ball.go: implement ball bouncing

--- ball.go
+++ ball.go
@@ -1,12 +1,66 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"math/rand"
+	"time"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+const (
+	// The lower it is, the faster the ball will move.
+	BALL_SPEED = 8
+)
+
+type Ball struct {
+	rect   rl.Rectangle
+	moving bool
+}
 
 var (
-	ball = rl.Rectangle{
-		X:      WINDOW_SIZE_X/2 - PADDLE_WIDTH/2,
-		Y:      WINDOW_SIZE_Y/2 - PADDLE_WIDTH/2,
-		Width:  PADDLE_WIDTH,
-		Height: PADDLE_WIDTH,
+	ball = Ball{
+		rect: rl.Rectangle{
+			Width:  PADDLE_WIDTH,
+			Height: PADDLE_WIDTH,
+		},
 	}
 )
+
+func (b *Ball) launch() {
+	pos := rand.Intn(LIMIT_BOTTOM_Y-LIMIT_TOP_Y) + LIMIT_TOP_Y
+	b.rect.X = WINDOW_SIZE_X/2 - PADDLE_WIDTH/2
+	b.rect.Y = float32(pos)
+	b.moving = true
+}
+
+func (b *Ball) move() {
+	var (
+		deltaX = float32(1)
+		deltaY = float32(1)
+
+		ticker = time.NewTicker(BALL_SPEED * time.Millisecond)
+	)
+
+	for range ticker.C {
+		x := b.rect.X + deltaX
+		y := b.rect.Y + deltaY
+
+		if !xLimitOk(x) {
+			deltaX *= -1
+		}
+
+		b.rect.X += deltaX
+
+		if !yLimitOk(y) {
+			deltaY *= -1
+		}
+
+		b.rect.Y += deltaY
+
+		if !b.moving {
+			ticker.Stop()
+		}
+	}
+
+	b.moving = false
+}
